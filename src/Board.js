4c8c7ee7f5b41b80b5ffe748229a31cd22052135
agarcluq <agarcluq@everis.com>
2021-04-30 13:36:54 +0200

    1 file modified

--- src/Board.js
+++ src/Board.js
@@ -8,28 +8,28 @@ export default function Board() {
       <div className="screen"> 
       </div>
       <div className="row">
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
+        <button className="col">7</button>
+        <button className="col">8</button>
+        <button className="col">9</button>
+        <button className="col">x</button>
       </div>
       <div className="row">
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
+        <button className="col">4</button>
+        <button className="col">5</button>
+        <button className="col">6</button>
+        <button className="col">-</button>
       </div>
       <div className="row">
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
+        <button className="col">1</button>
+        <button className="col">2</button>
+        <button className="col">3</button>
+        <button className="col">+</button>
       </div>
       <div className="row">
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
+        <button className="col">/</button>
+        <button className="col">0</button>
+        <button className="col">c</button>
+        <button className="col">=</button>
       </div>
     </div>
   );
