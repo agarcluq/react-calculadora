9e3a7360d459c1cb2622ab48cfccf996eed35996
agarcluq <agarcluq@everis.com>
2021-04-30 13:16:19 +0200

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
-
+import Board from "./Board"
 export default function App() {
   return (
     <div>
-      <h1>El ahorcado</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Calculadora</h1>
+        <Board/>
     </div>
   );
 }
---
+++ src/Board.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function Board() {
+  return (
+    <div className="board">
+      
+    </div>
+  );
+}
