1bfab166328b8b35c7606400029604f9e3c3efc8
agarcluq <agarcluq@everis.com>
2021-04-30 13:18:46 +0200

    1 file modified

--- src/Board.js
+++ src/Board.js
@@ -4,7 +4,30 @@ import "./style.css";
 export default function Board() {
   return (
     <div className="board">
+      <div className="pantalla">
       
+      </div>
+      <div className="row">
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+      </div>
+
+      <div className="row">
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+      </div>
+
+        <div className="row">
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+      </div>
+
     </div>
   );
 }
