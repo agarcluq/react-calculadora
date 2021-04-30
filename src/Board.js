f48a13d336446760e38fb5543351cfc43e315122
agarcluq <agarcluq@everis.com>
2021-04-30 13:30:13 +0200

    1 file modified, 2 added

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import Row from "/.Row"
 
 export default function Board() {
   return (
@@ -7,27 +8,10 @@ export default function Board() {
       <div className="screen">
       
       </div>
-      <div className="row">
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-      </div>
-
-      <div className="row">
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-      </div>
-
-        <div className="row">
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-      </div>
-
+      <Row/>
+      <Row/>
+      <Row/>
+      <Row/>
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function Row() {
+  return (
+      <div className="row">
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+      </div>
+  );
+}
